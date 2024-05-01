14cc9fb195966f70aba5ed80772c691d6bb61360
AndrewBedell10 <63424879+AndrewBedell10@users.noreply.github.com>
2024-05-01 08:59:49 -0400

    changing ' ' to " " for Company Name

--- AG_v2.py
+++ AG_v2.py
@@ -31,7 +31,7 @@ with col1:
     st.write("Company Name")
     company_name = st.selectbox(
         'Choose a Company',
-        df['Company Name'].sort_values().unique().tolist(),
+        df["Company Name"].sort_values().unique().tolist(),
         index=None,
         placeholder='start typing...'
     )
@@ -39,19 +39,19 @@ with col1:
 
 if company_name != None:
 
-    st.data_editor(df[df['Company Name'] == company_name].transpose())
+    st.data_editor(df[df["Company Name"] == company_name].transpose())
 
     col1, col2, col3 = st.columns(3)
     with col1:
-        st.metric(label="Company Market Cap (AUD)", value = '{:,.1f}'.format(df[df['Company Name'] == company_name][df.columns[2]].values[0]))
+        st.metric(label="Company Market Cap (AUD)", value = '{:,.1f}'.format(df[df["Company Name"] == company_name][df.columns[2]].values[0]))
 
     with col2:
-        st.metric(label="Average Daily Value Traded - 52 Weeks (AUD)", value = '{:,.1f}'.format(df[df['Company Name'] == company_name][df.columns[3]].values[0]))
+        st.metric(label="Average Daily Value Traded - 52 Weeks (AUD)", value = '{:,.1f}'.format(df[df["Company Name"] == company_name][df.columns[3]].values[0]))
 
     with col3:
         st.metric(label="""Cash from Investing Activities, Cumulative
     (0FQ, FY0, AUD)""",
-                  value='{:,.1f}'.format(df[df['Company Name'] == company_name][df.columns[15]].values[0]))
+                  value='{:,.1f}'.format(df[df["Company Name"] == company_name][df.columns[15]].values[0]))
 
 
-    st.write(f'Info: {df[df['Company Name'] == company_name]["Business Description"].values[0]}')
+    st.write(f'Info: {df[df["Company Name"] == company_name]["Business Description"].values[0]}')
